0ef1f7dfa49db58f94c7811dd6959d9dc008d646
Joshalosh <josh.jessopsmith@gmail.com>
2023-05-29 21:33:37 +1000

    Try to make collatz sequence break early

--- collatz.cpp
+++ collatz.cpp
@@ -1,21 +1,37 @@
 #include <iostream>
 
+#define Array_Count(array) (sizeof(array) / sizeof((array)[0]))
+
 int main()
 {
     int largest_count = 0;
     int largest_num = 0;
+    int largest_sequence[4096] = {};
 
-    for(int index = 2; index < 1000000; index++)
+    for(int index = 3; index < 100000; index += 2)
     {
+        int temp_sequence[4096] = {};
         int num = index;
         int count = 1;
+        int original_count;
+        bool is_done;
         while(num != 1)
         {
-            if(num == largest_num)
+            is_done = false;
+            for(int i = 0; i < largest_count; i++) 
             {
-                count += largest_count - 1;
-                break;
+                if(num == largest_sequence[i])
+                {
+                    original_count = count;
+                    count += largest_count - 1 - i;
+                    is_done = true;
+                    break;
+                }
             }
+
+            if(is_done)
+                break;
+
             if(num % 2 == 0)
             {
                 num /= 2;
@@ -25,11 +41,20 @@ int main()
                 num *= 3;
                 num += 1;
             }
+
+            temp_sequence[count] = num;
             count++;
         }
 
         if(count > largest_count)
         {
+            int sequence_count = is_done ? original_count : count;
+
+            temp_sequence[0] = index;
+            for(int i = 0; i < sequence_count; i++)
+            {
+                largest_sequence[i] = temp_sequence[i];
+            }
             largest_count = count;
             largest_num = index;
         }
