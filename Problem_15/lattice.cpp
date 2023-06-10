79316a891ba349025f38cd4861393207ec689d7b
Joshalosh <josh.jessopsmith@gmail.com>
2023-06-10 13:18:38 +1000

    Add custom helper functions

--- Problem_15/lattice.cpp
+++ Problem_15/lattice.cpp
@@ -9,28 +9,18 @@ struct Point {
 };
 
 Point **SetUpGrid(int size) {
-
-    // We need to add one to the size as we are setting up the nodes
-    // on a grid not the paths between. So that means a 2x2 grid is 
-    // made up of 3x3 nodes.
     int height = size + 1;
     int width  = size + 1;
-
     Point **grid = (Point **)malloc(height * sizeof(Point *));
     for (int i = 0; i < height; i++) {
         grid[i] = (Point *)malloc(width * sizeof(Point));
     }
 
-    // Initialise the grid and set up the pointers correctly
+    // Initialise the grid with points
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            // All nodes except the last node on the x axis are pointed to the
-            // next node in the array. This needs to stop happening by the second
-            // last node in the array, if not it will then point out of bounds
             grid[y][x].right = (x < width - 1)  ? &grid[y][x+1] : NULL;
-            
-            // Same on the y axis
-            grid[y][x].down   = (y < height - 1) ? &grid[y+1][x] : NULL;
+            grid[y][x].down  = (y < height - 1) ? &grid[y+1][x] : NULL;
         }
     }
 
@@ -38,43 +28,68 @@ Point **SetUpGrid(int size) {
 }
 
 void FreeGrid(Point **grid, int size) {
-    //need to make sure to account for the extra node
-    //that was allocated
     int height = size + 1;
     for (int i = 0; i < height; i++) {
         free(grid[i]);
     }
-
     free(grid);
 }
 
-void SearchGridForUniquePaths(Point *travel_node, uint64_t *path_count) {
+bool IsDigit(char c) {
+    bool result = (c >= '0' && c <= '9');
+    return result;
+}
+
+int StringToInt(char *str) {
+    int result = 0;
+    int sign = 1;
+    
+    if (*str == '-') {
+        sign = -1;
+        *str++;
+    }
+
+    while(IsDigit(*str)) {
+        result = result * 10 + (*str - '0');
+        *str++;
+    }
+
+    result *= sign;
+    return result;
+}
+
+void FindUniquePaths(Point *travel_node, uint64_t *path_count) {
     if (travel_node->right == NULL && travel_node->down == NULL) {
         (*path_count)++;
+        return;
     }
 
     if (travel_node->right != NULL) {
-        SearchGridForUniquePaths(travel_node->right, path_count);
+        FindUniquePaths(travel_node->right, path_count); 
     }
 
     if (travel_node->down != NULL) {
-        SearchGridForUniquePaths(travel_node->down, path_count);
+        FindUniquePaths(travel_node->down, path_count);
     }
 }
 
 int main(int argc, char **argv) {
-    if (argc == 2) {
+    if (argc == 2 && IsDigit(*argv[1])) {
 
-        int size = atoi(argv[1]);
+        int grid_size = StringToInt(argv[1]);
 
-        Point **grid = SetUpGrid(size);
+        Point **grid = SetUpGrid(grid_size);
         Point *travel_node = grid[0];
-        
-        uint64_t path_count = 0;
-        SearchGridForUniquePaths(travel_node, &path_count);
 
-        printf("The number of unique paths are: %llu\n", path_count);
+        uint64_t path_count = 0;
+        FindUniquePaths(travel_node, &path_count);
+
+        printf("The number of unique lattice paths for a %dx%d grid is:\n%llu\n", grid_size, grid_size, path_count);
+
+        free(grid, grid_size);
+
     } else {
-        ("You need to pass a grid size\n");
+        printf("You need to pass a grid size\n");
     }
 }
+
