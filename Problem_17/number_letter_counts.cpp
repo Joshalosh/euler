a6a257d35b32b0f8367bf5fee5356f5c85be8fb3
Joshalosh <josh.jessopsmith@gmail.com>
2023-09-19 07:48:50 +1000

    Fix indexing bug

--- Problem_17/number_letter_counts.cpp
+++ Problem_17/number_letter_counts.cpp
@@ -8,34 +8,34 @@ char *tens_buffer[] = {"error", "twenty", "thirty", "fourty", "fifty", "sixty", 
 static void StringifyNumber() {
     for (int num = 1; num <= 100; num++) {
         if (num < 10) {
-            printf("%s ", ones_buffer[num + 1]);
+            printf("%s ", ones_buffer[num - 1]);
         } else if (num >= 10 && num < 100) {
             if (num >= 10 && num < 20) {
                 int teens = num % 10;
-                printf("%s ", teens_buffer[teens + 1]);
+                printf("%s ", teens_buffer[teens - 1]);
             } else {
                 int tens = num / 10;
                 int ones = num % 10;
-                printf("%s ", tens_buffer[tens + 1]);
+                printf("%s ", tens_buffer[tens - 1]);
                 if (ones) {
-                    printf("%s ", ones_buffer[ones + 1]);
+                    printf("%s ", ones_buffer[ones - 1]);
                 }
             }
         } else if (num >= 100 && num < 1000) {
             int hundreds = num / 100;
             int tens = num % 100;
             int ones = tens % 10;
-            printf("%s hundred and ", ones_buffer[hundreds+1]);
+            printf("%s hundred and ", ones_buffer[hundreds-1]);
             if (tens >= 10 && tens < 20) {
                 tens = tens % 10;
-                printf("%s ", teens_buffer[tens + 1]);
+                printf("%s ", teens_buffer[tens - 1]);
             } else if (tens >= 20) {
-                printf("%s ", tens_buffer[(tens/10)+1]);
+                printf("%s ", tens_buffer[(tens/10)-1]);
                 if (ones) {
-                    printf("%s ", ones_buffer[ones + 1]);
+                    printf("%s ", ones_buffer[ones - 1]);
                 }
             } else {
-                printf("%s ", ones_buffer[ones + 1]); 
+                printf("%s ", ones_buffer[ones - 1]); 
             }
         }
     }
