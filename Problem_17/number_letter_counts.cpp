07c84eca624bb6be8674327b7e817be77669e564
Joshalosh <josh.jessopsmith@gmail.com>
2023-09-18 21:48:56 +1000

    Add enums to loop

--- Problem_17/number_letter_counts.cpp
+++ Problem_17/number_letter_counts.cpp
@@ -18,12 +18,16 @@ char *tens_buffer = {"error", "twenty", "thirty", "fourty", "fifty", "sixty", "s
 static void StringifyNumber() {
     for (int num = 1; num <= 100; num++) {
         int mod;
+        Power_Type power;
         if (num < 10) {
             mod = 1;
+            power = Power_Ones;
         } else if ( num >= 10 && num < 100) {
             mod = 10;
+            power = Power_Tens;
         } else if (num >= 100 && num < 1000) {
             mod == 100;
+            power = Power_Hundreds;
         }
     }
 }
