4c58357a04356c0611722319de005f3dc2f0eede
Joshalosh <josh.jessopsmith@gmail.com>
2023-09-23 09:18:07 +1000

    Add read file function

--- Problem_22/name_scores.cpp
+++ Problem_22/name_scores.cpp
@@ -7,5 +7,22 @@ struct File_Content {
     size_t size;
 };
 
-static char *ReadEntireFile(char *filename) {
+static FileContent ReadEntireFileAndNullTerminate(char *filename) {
+    FILE file;
+    File_Content result;
+
+    if (file) {
+        fopen(file, "r");
+        fseek(file, 0, SEEK_END);
+        result.size = ftell(file);
+        fseek(file, 0, SEEK_SET);
+
+        result.data = (char *)malloc(result.size + 1);
+        fread(result.data, result.size, 1, file);
+        fclose(file);
+
+        result.data[result.size] = '\0';
+    }
+
+    return result;
 }
