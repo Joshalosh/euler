084d0c92a1b46d0c7062cad5f94365d2818610e7
Joshalosh <josh.jessopsmith@gmail.com>
2023-12-05 15:21:28 +1100

    Make own copy array function

--- Problem_25/large_fib.cpp
+++ Problem_25/large_fib.cpp
@@ -4,14 +4,21 @@
 
 #define MAX_SIZE 1000
 
-static void AddLargeNumbers(int *array_one, int *array_two, int *result)
+
+static void AddLargeNumbers(int *array1, int *array2, int *result)
 {
     int carry = 0;
-
     for (int i = MAX_SIZE - 1; i >= 0; i--) {
-        int sum = array_one[i] + array_two[i] + carry;
+        int sum   = array1[i] + array2[i] + carry;
         result[i] = sum % 10;
         carry     = sum / 10;
+    }
+}
+
+static void CopyArray(int *source, int *dest)
+{
+    for (int i = 0; i < MAX_SIZE; i++) {
+        dest[i] = source[i];
     }
 }
 
@@ -19,9 +26,9 @@ int main()
 {
     int fib1[MAX_SIZE]       = {};
     int fib2[MAX_SIZE]       = {};
+    int fib_result[MAX_SIZE] = {};
     fib1[MAX_SIZE - 1]       = 1;
     fib2[MAX_SIZE - 1]       = 1;
-    int fib_result[MAX_SIZE] = {};
 
     int digit_count          = 1;
     int term                 = 2;
@@ -29,17 +36,16 @@ int main()
     while (digit_count < 1000) {
         AddLargeNumbers(fib1, fib2, fib_result);
 
-        memcpy(fib1, fib2, sizeof(fib1));
-        memcpy(fib2, fib_result, sizeof(fib2));
+        CopyArray(fib2, fib1);
+        CopyArray(fib_result, fib2);
 
-        // NOTE: Check if we've reached a new digit 
-        if (fib2[MAX_SIZE - digit_count - 1] != 0) {
+        if(fib2[MAX_SIZE - digit_count - 1] != 0)
+        {
             digit_count++;
         }
 
         term++;
     }
 
-    printf("First term with 1000 digits: %d\n", term);
+    printf("The first term with 1000 digits is: %d\n", term);
 }
-
